9822edbfe4ff243c002411fad5c9e83f4fc0590d
name8965 <name8965@naver.com>
2022-01-16 00:19:39 +0900

    연결리스트 문제 풀이
    연결리스트 문제 풀이

--- JangYunCheol/ch08/206_역순_연결_리스트.py
+++ JangYunCheol/ch08/206_역순_연결_리스트.py
@@ -1,5 +1,6 @@
 # Definition for singly-linked list.
 #219
+from typing import Optional
 
 class ListNode:
     def __init__(self, val=0, next=None):
@@ -7,8 +8,17 @@ class ListNode:
         self.next = next
 class Solution:
     def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        def reverse(node: ListNode, prev: ListNode = None):
-            return prev
+        prev = None #연결리스트의 이전 위치 초기는 마지막위치 이므로 None
+        cur = head  #사용할 변수 링크 선언 (일종의 temp)헤드값을 담아 놓음#초기 값 연결리스트 = 연결리스트는 헤드를 알면 전체를 알수있다
+        while cur:  #헤드(연결리스트)의 길이 만큼 while를 반복
+            next = cur.next  #다음 가야할 곳을 담아 놓음 일종의 Temp2
+            cur.next = prev  #헤드의 next에 이전리스트를 연결 반전시킴/N 1->2->3->N 이였던걸 N<-1 2->3->N 으로 바꿈
+            prev = cur       #이전리스트에 curr 일종에 cur 은 위치를 알려주는 역활
+            cur = next       #다음 가야할 곳에 cur 일종의 위치를 알려주는 아이를 보냄
+        return prev
 
-
-        return reverse()
+s = Solution()
+listn = ListNode(1,ListNode(2,ListNode(3,ListNode(4,ListNode(5)))))
+print(s.reverseList(ListNode(1,ListNode(2,ListNode(3,ListNode(4,ListNode(5)))))))
+print(listn)
+print(listn)
--- JangYunCheol/ch08/21_두_정렬_리스트의_병합.py
+++ JangYunCheol/ch08/21_두_정렬_리스트의_병합.py
@@ -1,12 +1,53 @@
 #p213
 from typing import List
-class Solution:
-    def rob(self, nums: List[int]) -> int:
+from typing import Optional
 
-        return 0
+
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+class Solution:
+    def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
+        # dummy =cur=ListNode(0) #반복문 방식
+        #
+        # while list1 and list2:
+        #     if list1.val<list2.val:
+        #         cur.next = list1
+        #         list1 = list1.next
+        #     else:
+        #         cur.next = list2
+        #         list2 = list2.next
+        #     cur=cur.next
+        # cur.next = list1 or list2
+        # return dummy.next
+
+
+        # 재귀방식 [124][134]
+        if list1 is None or list2 is None:  #base case 재귀 탈출용
+            return list1 or list2
+        if list1.val < list2.val: #1번 리스트가 더 작을때 1번 리스트에 다음번째에 재귀를 호출
+            list1.next = self.mergeTwoLists(list1.next,list2) #236
+            return list1
+        else:
+            list2.next = self.mergeTwoLists(list1,list2.next) #145
+            return list2 #2번 리스트가 더 작을땐 2번 리스트의 다음 번째를 넘겨주며 재귀를 호출
+
 
 
 
 
 s = Solution()
-print(s.rob([1, 4, 3, 2]))
+list1=ListNode(1,ListNode(2,ListNode(4)))
+list2=ListNode(1,ListNode(3,ListNode(4)))
+print(s.mergeTwoLists(ListNode(1,ListNode(2,ListNode(4))),ListNode(1,ListNode(3,ListNode(4)))))
+
+l1 = ListNode(1)
+l1.next = ListNode(2)
+l1.next.next = ListNode(4)
+
+print(l1)
+print(list2)
+print("------------")
+
